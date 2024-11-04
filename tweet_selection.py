ac25c0e1b0c59512fbaf4360765849b0a4fa7013
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-11-04 16:39:34 -0300

    feat: add tweets table

--- tweet_selection.py
+++ tweet_selection.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, current_app, url_for, jsonify
+import os
 
 tweet_selection_bp = Blueprint('tweet_selection', __name__)
 
 @tweet_selection_bp.route('/tweet-selection')
 def index():
     return render_template('tweet-selection.html')
+
+
+@tweet_selection_bp.route('/save_tweets_csv', methods=['POST'])
+def save_csv():
+    data = request.get_json()
+    csv_content = data.get("csv", "")
+
+    csv_filename = 'tweets.csv'
+    csv_path = os.path.join(current_app.config['PRODUCT_CSV_FOLDER'], csv_filename)
+
+    with open(csv_path, 'w', encoding='utf-8', newline='') as f:
+        f.write(csv_content)
+
+    download_url = url_for('static', filename=f'products_csv/{csv_filename}', _external=True)
+
+    return jsonify({'download_url': download_url})
