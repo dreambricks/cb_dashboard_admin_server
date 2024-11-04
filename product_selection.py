f089a542c485133e20066d377a378375e94f41a5
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-11-04 16:00:49 -0300

    fix: do not change original index on tables

--- product_selection.py
+++ product_selection.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from flask import Blueprint, render_template, current_app
+from flask import Blueprint, render_template, current_app, request, url_for, jsonify
 import os
 
 
@@ -9,3 +8,20 @@ product_selection_bp = Blueprint('product_selection', __name__)
 @product_selection_bp.route('/product-selection')
 def index():
     return render_template('product-selection.html')
+
+
+@product_selection_bp.route('/save_csv', methods=['POST'])
+def save_csv():
+    data = request.get_json()
+    csv_content = data.get("csv", "")
+
+    csv_filename = 'produtos.csv'
+    csv_path = os.path.join(current_app.config['PRODUCT_CSV_FOLDER'], csv_filename)
+
+    with open(csv_path, 'w', encoding='utf-8', newline='') as f:
+        f.write(csv_content)
+
+    download_url = url_for('static', filename=f'products_csv/{csv_filename}', _external=True)
+
+    return jsonify({'download_url': download_url})
+
