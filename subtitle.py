6e6a554deb3c842c3063ab5423f9ec19530412c0
Julio Cesar Balbino de Morais <julio4110@hotmail.com>
2024-11-18 20:01:21 -0300

    feat: add fake words to word-selection

--- subtitle.py
+++ subtitle.py
@@ -25,6 +25,21 @@ def serve_subtitles():
     return send_from_directory(subtitles_folder_path, subtitles_tsv_filename)
 
 
+@subtitle_bp.route('/dynamic/categorias')
+def serve_categories():
+    subtitles_folder_path = current_app.config['CATEGORIES_FOLDER']
+    subtitles_tsv_filename = None
+
+    for file in os.listdir(subtitles_folder_path):
+        if file.endswith('.tsv'):
+            subtitles_tsv_filename = file
+            break
+
+    if subtitles_tsv_filename is None:
+        return "No TSV file found in the directory.", 404
+
+    return send_from_directory(subtitles_folder_path, subtitles_tsv_filename)
+
 
 @subtitle_bp.route('/save_subtitle', methods=['POST'])
 def save_tsv():
@@ -59,4 +74,34 @@ def save_tsv():
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 
+@subtitle_bp.route('/save_categories', methods=['POST'])
+def save_subtitle():
+    try:
+        # Valida se o JSON recebido contém a chave 'tsv'
+        data = request.json.get('tsv')
+        if not data:
+            return jsonify({"error": "No TSV data provided."}), 400
 
+        # Obtém o caminho da pasta onde os arquivos de legenda estão localizados
+        subtitles_folder_path = current_app.config['CATEGORIES_FOLDER']
+        subtitles_tsv_filename = None
+
+        # Encontra o primeiro arquivo .tsv na pasta
+        for file in os.listdir(subtitles_folder_path):
+            if file.endswith('.tsv'):
+                subtitles_tsv_filename = file
+                break
+
+        if subtitles_tsv_filename is None:
+            return jsonify({"error": "No TSV file found in the subtitles folder."}), 404
+
+        subtitles_tsv_path = os.path.join(subtitles_folder_path, subtitles_tsv_filename)
+
+        # Tenta escrever os dados no arquivo
+        with open(subtitles_tsv_path, 'w', encoding='utf-8') as file:
+            file.write(data)
+
+        return jsonify({"message": "File saved successfully."}), 200
+
+    except Exception as e:
+        return jsonify({"error": f"An error occurred: {str(e)}"}), 500
